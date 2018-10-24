bea63f6ecf2bd4885bc61d86c69a3f59a69ca032
Clyde <clydeponteville@hotmail.fr>
2018-10-24 09:48:30 +0200

    add try catch

--- 12-pdo/index.php
+++ 12-pdo/index.php
@@ -14,12 +14,17 @@
         $dbname = 'pizzastore';
         $user = 'root';
         $pwd = '';
-        $db = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=UTF8' ,$user, $pwd,[
-            //Affiche les erreur sql
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
-            // On récupère tous les résultats en tableau associatif
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ]);
+        try{
+            $db = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=UTF8' ,$user, $pwd,[
+                //Affiche les erreur sql
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+                // On récupère tous les résultats en tableau associatif
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
+        }catch(Exception $e){
+            echo $e->getMessage();
+            header('Location: https://www.google.fr/search?q='.$e->getMessage());
+        }
         $query = $db->query('SELECT * FROM pizza');
         $result = $query->fetchAll();
 
