f78fd220163f725a21eb5f17141fdb53b42733f6
Clyde <clydeponteville@hotmail.fr>
2018-10-24 17:28:02 +0200

    add script

---
+++ 13-mini_proj/assets/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(document).ready(function(){
+
+    // $.ajax({
+    //     url: 'pizza_single.php',
+    //     method: 'get',
+        
+    //     success : function(data){                
+                        
+    //         console.log(data);                
+    //     },
+    //     error: function(e) {        
+    //         console.log(e+" - Désolé, aucun résultat trouvé.");
+    //     }
+    // }); 
+});
