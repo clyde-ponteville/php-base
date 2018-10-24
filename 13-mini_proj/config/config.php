9330e0485ebd44f103e24eff570b8cd6b546db72
Clyde <clydeponteville@hotmail.fr>
2018-10-24 16:56:44 +0200

    config variables globales

---
+++ 13-mini_proj/config/config.php
@@ -0,0 +1,20 @@
+<?php 
+
+/* 
+------------------------------------------------------
+-------------------Config File------------------------
+------------------------------------------------------
+
+Ce fichier contiendra nos variables "globales" pour notre site.
+Titre du site, titre de la page, page courante, ....
+
+*/
+
+$siteName= 'Pizza Store';
+
+// page courante et titre de la balise
+$currentPageTitle = null;
+$currentPageUrl = basename($_SERVER['REQUEST_URI'], '.php');
+
+
+?>
